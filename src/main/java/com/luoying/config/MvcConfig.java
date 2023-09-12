7f8e7ad19d1428de1440aabb537d78e3dde2c5e8
LUOYINGDEHUIHEN <1574925401@qq.com>
2023-09-12 13:51:38 +0800

    补充后端镜像制作文件

--- src/main/java/com/luoying/config/MvcConfig.java
+++ src/main/java/com/luoying/config/MvcConfig.java
@@ -22,19 +22,4 @@ public class MvcConfig implements WebMvcConfigurer {
                 "/user/register"
         ).order(1);
     }
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        //设置允许跨域的路径
-        registry.addMapping("/**")
-                //设置允许跨域请求的域名
-                //当**Credentials为true时，**Origin不能为星号，需为具体的ip地址【如果接口不带cookie,ip无需设成具体ip】
-                .allowedOrigins("http://localhost:8000", "http://192.168.253.128:80")
-                //是否允许证书 不再默认开启
-                .allowCredentials(true)
-                //设置允许的方法
-                .allowedMethods("*")
-                .allowedHeaders("Authorization")
-                //跨域允许时间
-                .maxAge(3600);
-    }
 }

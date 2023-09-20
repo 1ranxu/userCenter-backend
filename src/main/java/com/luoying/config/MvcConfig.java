3d8ba071749765bc63d6740762c0e5914b7f7cee
LUOYINGDEHUIHEN <1574925401@qq.com>
2023-09-20 14:47:12 +0800

    修改全局拦截器

--- src/main/java/com/luoying/config/MvcConfig.java
+++ src/main/java/com/luoying/config/MvcConfig.java
@@ -1,10 +1,8 @@
 package com.luoying.config;
 
 import com.luoying.utils.LoginInterceptor;
-import com.luoying.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,10 +17,10 @@ public class MvcConfig implements WebMvcConfigurer {
         // 对swagger的请求不进行拦截
         String[] excludePatterns = new String[]{"/swagger-resources/**", "/swagger-resources","/webjars/**", "/v2/**", "/swagger-ui.html/**",
                 "/api", "/api-docs", "/api-docs/**", "/doc.html","/doc.html#/**"};
-        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
-        registry.addInterceptor(new LoginInterceptor()).excludePathPatterns(
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns(
                 "/user/login",
-                "/user/register"
+                "/user/register",
+                "/user/searchByTags"
         ).excludePathPatterns(excludePatterns).order(1);
     }
 }
--- src/main/java/com/luoying/utils/LoginInterceptor.java
+++ src/main/java/com/luoying/utils/LoginInterceptor.java
@@ -1,22 +1,27 @@
 package com.luoying.utils;
 
+import com.luoying.model.dto.UserDTO;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static com.luoying.constant.UserConstant.USER_LOGIN_STATE;
+
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
-        if (UserHolder.getUser() == null) {
-            // 没有，需要拦截，设置状态码
+        UserDTO userDTO = (UserDTO) request.getSession().getAttribute(USER_LOGIN_STATE);
+        // 1.判断是否需要拦截
+        if (userDTO == null) {
+            // 用户未登录，需要拦截，设置状态码
             response.setStatus(401);
             // 拦截
             return false;
         }
         // 有用户，则放行
+        UserHolder.saveUser(userDTO);
         return true;
     }
 }

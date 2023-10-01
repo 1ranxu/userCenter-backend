b9448e225da5f032eafc809a541f89b880a551f6
LUOYINGDEHUIHEN <1574925401@qq.com>
2023-10-01 17:22:14 +0800

    update 跨域配置

--- src/main/java/com/luoying/config/MvcConfig.java
+++ src/main/java/com/luoying/config/MvcConfig.java
@@ -2,16 +2,12 @@ package com.luoying.config;
 
 import com.luoying.utils.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.annotation.Resource;
-
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    @Resource
-    private StringRedisTemplate stringRedisTemplate;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 对swagger的请求不进行拦截
@@ -23,5 +19,20 @@ public class MvcConfig implements WebMvcConfigurer {
                 "/user/searchByTags",
                 "/user/recommend"
         ).excludePathPatterns(excludePatterns).order(0);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //设置允许跨域的路径
+        registry.addMapping("/**")
+                //设置允许跨域请求的域名
+                //当**Credentials为true时，**Origin不能为星号，需为具体的ip地址【如果接口不带cookie,ip无需设成具体ip】
+                .allowedOrigins("http://localhost:5173", "http://localhost:8000","http://localhost:3000")
+                //是否允许证书 不再默认开启
+                .allowCredentials(true)
+                //设置允许的方法
+                .allowedMethods("*")
+                //跨域允许时间
+                .maxAge(3600);
     }
 }
